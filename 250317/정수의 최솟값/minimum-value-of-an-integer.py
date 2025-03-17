9c3006fd3b4ba5af8f2872eaca065f8a92f34553
최다녕 <112795980+ddny226@users.noreply.github.com>
2025-03-17 21:53:12 +0900

    Title: 정수의 최솟값, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250317/정수의 최솟값/minimum-value-of-an-integer.py
+++ 250317/정수의 최솟값/minimum-value-of-an-integer.py
@@ -1,20 +1,14 @@
 a, b, c = map(int, input().split())
 
-def min(a, b, c):
-    if a > b:
-        if b > c:
-            return c
-        else:
-            return b
-    elif b > c:
-        if c > a:
-            return a
-        else: return c
-    elif c > a:
-        if b > a:
-            return a
-        else:
-            return b
-    
+def my_min(a, b, c):
+    # 첫 번째 값을 최솟값으로 가정
+    minimum = a
+    # b와 비교
+    if b < minimum:
+        minimum = b
+    # c와 비교
+    if c < minimum:
+        minimum = c
+    return minimum
 
-print(min(a,b,c))
+print(my_min(a, b, c))
