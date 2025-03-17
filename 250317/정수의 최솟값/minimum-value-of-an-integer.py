564528590c3f664d2e5fa13ffee41bbb608aa65f
최다녕 <112795980+ddny226@users.noreply.github.com>
2025-03-17 21:51:26 +0900

    Title: 정수의 최솟값, Time: 39ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250317/정수의 최솟값/minimum-value-of-an-integer.py
+++ 250317/정수의 최솟값/minimum-value-of-an-integer.py
@@ -6,6 +6,15 @@ def min(a, b, c):
             return c
         else:
             return b
-    else: return a
+    elif b > c:
+        if c > a:
+            return a
+        else: return c
+    elif c > a:
+        if b > a:
+            return a
+        else:
+            return b
+    
 
 print(min(a,b,c))
