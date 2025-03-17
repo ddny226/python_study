2c674a6ad4277419e4fa1df0522d93f15adb4855
최다녕 <112795980+ddny226@users.noreply.github.com>
2025-03-17 22:42:53 +0900

    Title: 함수를 이용한 윤년 판별, Time: 81ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250317/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 250317/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -2,10 +2,10 @@ y = int(input())
 
 def yoon_year(y):
     if y % 4 == 0:
-        return True
+        return true
     elif y % 100 == 0 and y % 400 != 0:
-        return True
+        return true
     else:
-        return False
+        return false
 
 print(yoon_year(y))
